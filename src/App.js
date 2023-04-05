00cb829e2cc33c0cb56698d0d4b8061ed5a30729
NayoBaez <naomi.singh@hotmail.com>
2023-04-05 16:12:50 -0400

    feature:create all functions in app component

--- src/App.js
+++ src/App.js
@@ -5,19 +5,62 @@ import confetti from "react-confetti";
 import "./style.css";
 
 function App() {
-  const [dice, setDice] = useState();
-  const [tenzies, setTenzies] = useState();
+  const [dice, setDice] = useState(allNewDice());
+  const [tenzies, setTenzies] = useState(false);
+
+  function generateNewDice() {
+    return {
+      value: Math.ceil(Math.random * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generateNewDice());
+    }
+  }
+
+  function rollDice() {
+    if (!tenzies) {
+      setDice((oldDice) =>
+        oldDice.map((die) => {
+          return die.isHeld ? die : generateNewDice();
+        })
+      );
+    }
+  }
+
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
+
+  const diceElements = dice.map((die) => (
+    <Dice
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
 
   return (
     <div className="App">
-      <Dice />
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
         Roll until all dice are the same. Click each die to freeze it at its
         current value between rolls.
       </p>
-      <div ClassName="dice-container"></div>
-      <button className="roll-dice">Roll</button>
+      <div ClassName="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
     </div>
   );
 }
